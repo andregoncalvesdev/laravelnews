4d7ee52a344ce9b0e83503ca876bfc8dc304a233
André Gonçalves <andregoncalves@tekzenit.com>
2016-09-12 12:27:23 +0100

    add tag

--- src/LaravelNewsServiceProvider.php
+++ src/LaravelNewsServiceProvider.php
@@ -22,6 +22,10 @@ class LaravelNewsServiceProvider extends ServiceProvider
     $this->publishes([
       __DIR__.'/migrations' => database_path('migrations')
     ], 'migrations');
+
+    $this->publishes([
+      __DIR__.'/../config/package.php' => config_path('package.php')
+    ], 'laravelnews'); 
   }
 
   /**
